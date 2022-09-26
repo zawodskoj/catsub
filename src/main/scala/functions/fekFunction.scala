0715bd75b89fab3a9c675fa00fcc3a72ab06f3eb
невидимка <bessonov.do@perfema.com>
2022-09-26 23:18:11 +0300

    alex chat check if configured

--- src/main/scala/functions/fekFunction.scala
+++ src/main/scala/functions/fekFunction.scala
@@ -8,12 +8,13 @@ import tg.api._
 
 object fekFunction {
   val alexId: Option[Long] = sys.env.get("ALEX_ID").flatMap(_.toLongOption)
+  val alexChatId: Option[Long] = sys.env.get("ALEX_CHAT_ID").flatMap(_.toLongOption)
 
   def resource(implicit b: SttpBackend[IO, Any]): Resource[IO, BotFunction] = Resource.pure[IO, BotFunction] { update =>
     val hasUrl = update.message.toList.flatMap(_.entities.getOrElse(Nil)).exists(x => x.`type` == "url")
     val hasPhoto = update.message.flatMap(_.photo).nonEmpty
 
-    val isAlex = update.message.exists(m => alexId.contains(m.from.id))
+    val isAlex = update.message.exists(m => alexId.contains(m.from.id) && alexChatId.forall(_ == m.chat.id))
     val ambient = if (isAlex) 0.1 else 0
     val userMultiplier = if (isAlex) 0.7 else 0.3
     val probability = ((if (hasUrl) 0.9 else 0) + (if (hasPhoto) 0.3 else 0)) * userMultiplier + ambient
